3a604170825262d69f881c1138251874dd4f9e53
CODINGSCARESME <Rayanmohamed.0891@gmail.com>
2026-05-03 17:48:25 +0100

    Updated setup.php

--- server/public/setup.php
+++ server/public/setup.php
@@ -4,12 +4,8 @@ $username = "root";
 $password = "root";
 $database = "carpool";
 
-// creating a connection.
 $conn = new mysqli($host,$username,$password,$database);
-
-// checking the connection.
 if ($conn ->connect_error) {
     die("$database" . $conn ->connect_error);
 }
-echo "Connected successfully";
 ?>
